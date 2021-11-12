51ebd1d2fc337e4abaa6c08f0c6e917281632899
prashant169 <prashantphad2018@gmail.com>
2021-11-12 09:09:33 -0800

    Added: Creating a database in mysql Shell and connect it

---
+++ src/main/java/com/bridgelabz/employeepayrolejdbc/EmployeePayRoll.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrolejdbc;
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
+    }
+}
